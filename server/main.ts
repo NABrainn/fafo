f4af52a620b55f0e40d730c5f90c58c4f965e431
NAbrain <michal2002.60@gmail.com>
2025-03-23 10:26:19 +0100

    add postgres

--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,7 @@
 // @ts-types="npm:@types/express@4.17.15"
 
 import express from "npm:express@4.21.2";
+import { Sequelize, DataTypes } from 'sequelize';
 
 const app = express();
 
@@ -10,3 +11,15 @@ app.get("/", (req, res) => {
 
 app.listen(8000);
 console.log(`Server is running on http://localhost:8000`);
+
+const sequelize = new Sequelize('blog', 'postgres', 'postgres', {
+  host: 'localhost',
+  dialect: 'postgres'
+})
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
