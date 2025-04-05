f254be2225dc7db521fb7127e6f2cc79c1e278d8
NAbrain <michal2002.60@gmail.com>
2025-04-05 15:35:06 +0200

    -implement initial post service methods -hook up service to the post page feature

--- client/chicken-app/src/features/post/post.service.ts
+++ client/chicken-app/src/features/post/post.service.ts
@@ -1,8 +1,43 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, httpResource } from '@angular/common/http';
+import { computed, inject, Injectable, WritableSignal } from '@angular/core';
+import { API_URL } from '../../environment';
+import { catchError, of, throwError } from 'rxjs';
+
+export type Post = {
+  id: number
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-  
+
+  http = inject(HttpClient)
+  URL = `${API_URL}/posts`
+
+  findAll() {
+    return httpResource<Post[]>(() => this.URL)
+  }
+
+  findById(id: number) {
+    return httpResource<Post[]>(() => `${this.URL}/${id}`)
+  }
+
+  save(post: Post) {
+    return this.http.post<Post>(this.URL, post)
+  }
+
+  deleteById(id: number) {
+    return this.http.delete<void>(`${this.URL}/${id}`).pipe(
+      catchError(err => throwError(
+        () => err
+      ))
+    )
+  }
+
+  update(post: Post) {
+    return this.http.put<Post>(this.URL, post).pipe(
+      catchError(err => of(undefined))
+    )
+  }
 }
