059cbd24ad18398a74482e701ebb234f5bcfe9b9
NAbrain <michal2002.60@gmail.com>
2025-04-05 12:16:31 +0200

    fill page-not-found content

--- client/chicken-app/src/app/components/page-not-found/page-not-found.component.ts
+++ client/chicken-app/src/app/components/page-not-found/page-not-found.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './page-not-found.component.html'
 })
 export class PageNotFoundComponent {
-
+  router = inject(Router)
+  route = this.router.url
 }
