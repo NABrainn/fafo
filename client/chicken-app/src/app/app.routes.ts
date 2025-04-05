701a22448bec842ef3c5d12ba141344c981741df
NAbrain <michal2002.60@gmail.com>
2025-04-05 15:33:51 +0200

    update routes

--- client/chicken-app/src/app/app.routes.ts
+++ client/chicken-app/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { PostListComponent } from './components/post-list/post-list.component';
+import { PostPageComponent } from '../features/post/post-page/post-page.component';
 
 export const routes: Routes = [
-    {path: 'posty', component: PostListComponent},
+    {path: 'posty', component: PostPageComponent},
     {path: '', redirectTo: '/posty', pathMatch: 'full'},
     {path: '**', loadComponent: () => import('./components/page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent)}
 ];
