6f90ab6a38707d729f3531620c6405c69123f221
NAbrain <michal2002.60@gmail.com>
2025-04-09 14:10:30 +0200

    refactor into classes

--- server/src/database/repository/commentRepository.ts
+++ server/src/database/repository/commentRepository.ts
@@ -1,35 +1,37 @@
 import { db } from "../db.ts";
 import { Comment, comments } from "../schema/comments.ts";
 import { eq } from "drizzle-orm/expressions";
-import { CrudRepository } from "./crudRepository.ts";
-import { BlogPost } from "../schema/blogPosts.ts";
-import { User } from "../schema/users.ts";
 
-export const commentRepository: CrudRepository<Comment> = {
-    findById: async (id: number) => {
-        const found = await db.select().from(comments).where(eq(comments.id, id));
+export class CommentRepository {
+
+    private pool!: typeof db
+
+    constructor(pool: typeof db) {
+        this.pool = pool
+    }
+
+    async findById(id: number) {
+        const found = await this.pool.select().from(comments).where(eq(comments.id, id));
         return found[0]
-    },
-    findAll: async () => {
+    }
+    async findAll() {
         return await db.select().from(comments)
-    },
-    create: async (data: Comment, blogPost: BlogPost, author: User) => {
-        const created = await db.insert(comments).values({
+    }
+    async create(data: Comment) {
+        const created = await this.pool.insert(comments).values({
         ...data,
-        blogPostId: blogPost.id,
-        authorId: author.id
         }).returning();
         return created[0]
-    },
-    updateById: async (id: number, data: Comment) => {
-        const updated = await db.update(comments).set({
+    }
+    async updateById (id: number, data: Comment) {
+        const updated = await this.pool.update(comments).set({
         ...comments,
         content: data.content
         }).where(eq(comments.id, id)).returning();
         return updated[0]
-    },
-    deleteById: async (id: number) => {
-        return await db.delete(comments).where(eq(comments.id, id))
+    }
+    async deleteById (id: number) {
+        return await this.pool.delete(comments).where(eq(comments.id, id))
     }
 }
 
--- server/src/database/repository/userRepository.ts
+++ server/src/database/repository/userRepository.ts
@@ -1,38 +1,41 @@
 import { User, users } from "../schema/users.ts";
-import { CrudRepository } from "./crudRepository.ts";
 import { db } from "../db.ts";
 import { eq } from "drizzle-orm/expressions";
 
-interface UserRepository extends CrudRepository<User> {
-  findByEmail(email: string): Promise<User>,
-  verifyUser(id: number):  Promise<User>
-}
+export class UserRepository {
 
-export const userRepository: UserRepository = {
-  findById: async (id: number) => {
-    const found = await db.select().from(users).where(eq(users.id, id));
+  pool!: typeof db;
+
+  constructor(pool: typeof db) {
+    this.pool = pool;
+  }
+  
+  async findById(id: number) {
+    const found = await this.pool.select().from(users).where(eq(users.id, id));
     return found[0];
-  },
-  findAll: async () => {
-    return await db.select().from(users);
-  },
-  create: async (data: User) => {
-    const created = await db.insert(users).values(data).returning();
+  }
+
+  async findAll() {
+    return await this.pool.select().from(users);
+  }
+  async create(data: Extract<User, typeof users.$inferInsert>) {
+    const created = await this.pool.insert(users).values(data).returning();
     return created[0];
-  },
-  updateById: async (id: number, data: User) => {
-    const updated = await db.update(users).set(data).where(eq(users.id, id)).returning();
+  }
+  async updateById(id: number, data: User) {
+    const updated = await this.pool.update(users).set(data).where(eq(users.id, id)).returning();
     return updated[0];
-  },
-  deleteById: async (id: number) => {
-    return await db.delete(users).where(eq(users.id, id));
-  },
-  findByEmail: async (email: string): Promise<User> => {
-    const found = await db.select().from(users).where(eq(users.email, email));
+  }
+  async deleteById(id: number) {
+    return await this.pool.delete(users).where(eq(users.id, id));
+
+  }
+  async findByEmail(email: string) {
+    const found = await this.pool.select().from(users).where(eq(users.email, email));
     return found[0]
-  },
-  verifyUser: async (id: number): Promise<User> => {
-    const verified = await db.update(users).set({verified: true}).where(eq(users.id, id)).returning()
+  }
+  async verifyUser(id: number) {
+    const verified = await this.pool.update(users).set({verified: true}).where(eq(users.id, id)).returning()
     return verified[0]
   }
 }
