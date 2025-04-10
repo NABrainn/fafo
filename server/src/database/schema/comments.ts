3683c4cc2157f1b23933764163472ad86df1fef1
NAbrain <michal2002.60@gmail.com>
2025-04-10 13:30:42 +0200

    remove createdBy field

--- server/src/database/schema/comments.ts
+++ server/src/database/schema/comments.ts
@@ -1,31 +1,26 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-import { relations } from "drizzle-orm/relations";
 import { blogPosts } from "./blogPosts.ts";
 import { users } from "./users.ts";
+import { relations } from "drizzle-orm/relations";
 
-export type Comment = typeof comments.$inferSelect
+export type Comment = typeof comments.$inferSelect | typeof comments.$inferInsert
 
 export const comments = pgTable('comments', {
     id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
     content: varchar('content', {length: 50}).notNull(),
-    createdBy: integer('createdBy').notNull(),
 
     authorId: integer('author_id').references(() => users.id, {onDelete: 'cascade'}).notNull(),
     blogPostId: integer('blog_post_id').references(() => blogPosts.id).notNull(),
     parentCommentId: integer('parent_comment_id')
 })
 
-export const commentsRelations = relations(comments, ({ one }) => ({
-    author: one(users, {
-        fields: [comments.authorId],
-        references: [users.id]
+export const commentsRelations = relations(comments, ({one}) => ({
+    author: one(users, { 
+        fields: [comments.authorId], 
+        references: [users.id] 
     }),
-    blogPost: one(blogPosts, {
-        fields: [comments.blogPostId],
-        references: [blogPosts.id]
+    blogPost: one(blogPosts, { 
+        fields: [comments.blogPostId], 
+        references: [blogPosts.id] 
     }),
-    parentComment: one(comments, {
-        fields: [comments.parentCommentId],
-        references: [comments.id]
-    })
 }))
