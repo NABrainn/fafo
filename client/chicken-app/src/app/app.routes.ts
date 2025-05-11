d42e2db3e2a795622fabff44c05324bab0ab4f5c
NAbrain <michal2002.60@gmail.com>
2025-05-11 19:52:21 +0200

    remove auth guard from posts list

--- client/chicken-app/src/app/app.routes.ts
+++ client/chicken-app/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { authGuard } from './auth/auth.guard';
 export const routes: Routes = [
     {path: 'posty', loadComponent: () => import('./features/post/components/post-list/post-list.component').then(c => c.PostListComponent), children: [
         {path: 'nowy', loadComponent: () => import('./features/post/components/post-form/post-form.component').then(c => c.PostFormComponent), canActivate: [authGuard]},
-    ], canActivate: [authGuard]},
+    ]},
     {path: 'posty/:id', loadComponent: () => import('./features/post/components/post-page/post-page.component').then(c => c.PostPageComponent)},
     {path: 'rejestracja', loadComponent: () => import('./auth/components/register-form/register-form.component').then(c => c.RegisterFormComponent)},
     {path: 'logowanie', loadComponent: () => import('./auth/components/login-form/login-form.component').then(c => c.LoginFormComponent)},
