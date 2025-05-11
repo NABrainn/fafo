c4591980013d3900b2c80316c0cbc9bc09e5490f
NAbrain <michal2002.60@gmail.com>
2025-05-11 19:54:27 +0200

    add blog post service

---
+++ client/chicken-app/src/app/features/post/service/post.service.spec.ts
@@ -0,0 +1,5 @@
+import { provideHttpClient } from "@angular/common/http";
+import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+
+
