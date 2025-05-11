2827afbacae5b0b0c2abc38f682154f9d8d16431
NAbrain <michal2002.60@gmail.com>
2025-05-11 19:54:06 +0200

    integrate comment posting

--- client/chicken-app/src/app/features/post/components/post-page/post-page.component.ts
+++ client/chicken-app/src/app/features/post/components/post-page/post-page.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
+import { BlogPost, PostService, SelectBlogPost } from '../../service/post.service';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { PostCommentComponent } from '../post-comment/post-comment.component';
+import { CommentService, InsertComment, SelectComment } from '../../service/comment.service';
+import { httpResource } from '@angular/common/http';
 
 @Component({
   selector: 'app-post-page',
-  imports: [],
+  imports: [
+    PostCommentComponent
+  ],
   templateUrl: './post-page.component.html',
   host: {
     class: 'grow-1 bg-primary'
@@ -10,4 +17,31 @@ import { Component } from '@angular/core';
 })
 export class PostPageComponent {
 
+  postService = inject(PostService);
+  commentService = inject(CommentService)
+  route = inject(ActivatedRoute);
+
+  #post = signal<SelectBlogPost | undefined>(undefined);
+  post = computed(() => this.#post())
+
+  #comments = signal<SelectComment[]>([]);
+  comments = computed(() => this.#comments());
+
+  onCommentSaved(id: number | undefined) {
+    if(id)
+      this.commentService.findAllCommentsByBlogId(id).subscribe((data) => {
+        this.#comments.set(data)
+      })    
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      const id = params.get('id');
+      if(id)
+        this.postService.findById(Number(id)).subscribe((data: SelectBlogPost) => {
+          this.#post.set(data) 
+          this.#comments.set(data.comments);
+        })
+    });
+  }
 }
