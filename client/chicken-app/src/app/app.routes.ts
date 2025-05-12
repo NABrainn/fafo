d5edae4d1061671605c449c352178dac8310f0cb
NAbrain <michal2002.60@gmail.com>
2025-05-12 15:13:12 +0200

    update imports

--- client/chicken-app/src/app/app.routes.ts
+++ client/chicken-app/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './auth/auth.guard';
+import { authGuard } from './core/auth/auth.guard';
 
 export const routes: Routes = [
     {path: 'posty', loadComponent: () => import('./features/post/components/post-list/post-list.component').then(c => c.PostListComponent), children: [
         {path: 'nowy', loadComponent: () => import('./features/post/components/post-form/post-form.component').then(c => c.PostFormComponent), canActivate: [authGuard]},
     ]},
     {path: 'posty/:id', loadComponent: () => import('./features/post/components/post-page/post-page.component').then(c => c.PostPageComponent)},
-    {path: 'rejestracja', loadComponent: () => import('./auth/components/register-form/register-form.component').then(c => c.RegisterFormComponent)},
-    {path: 'logowanie', loadComponent: () => import('./auth/components/login-form/login-form.component').then(c => c.LoginFormComponent)},
+    {path: 'rejestracja', loadComponent: () => import('./core/auth/components/register-form/register-form.component').then(c => c.RegisterFormComponent)},
+    {path: 'logowanie', loadComponent: () => import('./core/auth/components/login-form/login-form.component').then(c => c.LoginFormComponent)},
     {path: '', redirectTo: '/posty', pathMatch: 'full'},
     {path: '**', loadComponent: () => import('./core/page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent)}
 ];
