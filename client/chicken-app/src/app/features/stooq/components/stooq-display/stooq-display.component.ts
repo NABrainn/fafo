84807f38826ff44bbf6d009331c178cf86234012
NAbrain <michal2002.60@gmail.com>
2025-05-13 11:33:33 +0200

    placeholder

--- client/chicken-app/src/app/features/stooq/components/stooq-display/stooq-display.component.ts
+++ client/chicken-app/src/app/features/stooq/components/stooq-display/stooq-display.component.ts
@@ -1,6 +1,5 @@
 import {Component, inject} from '@angular/core';
 import {StooqService} from '../../stooq.service';
-import {ChickenFactService} from '../../../chicken-facts/chicken-fact.service';
 
 @Component({
   selector: 'app-stooq-display',
