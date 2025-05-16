98f27f80338034df67633f450dd1b3e3b0daafed
NAbrain <michal2002.60@gmail.com>
2025-05-16 17:04:59 +0200

    service for adding image

--- server/src/controller/image/imageService.ts
+++ server/src/controller/image/imageService.ts
@@ -1,8 +1,22 @@
-export async function saveImage(file: File) {
-    const decoder = new TextDecoder('utf-8');
-    const path = Deno.cwd()
-    const imageData =  await file.arrayBuffer();
-    const imageBytes = new Uint8Array(imageData);
+import {ImageRepository} from "../../database/repository/imageRepository.ts";
+import {db} from "../../database/database.ts";
+import {ImageForm} from "../imageController.ts";
 
+const imageRepository = new ImageRepository(db)
+
+export async function uploadImage(image: ImageForm) {
+    const fileName = `${crypto.randomUUID()}-${image.imageData.name}`
+    const filePath = `./uploads/${fileName}`;
+    const fileSize = image.imageData.size
+    const contentType = image.imageData.type
+
+    const arrayBuffer = await image.imageData.arrayBuffer();
+    await Deno.writeFile(filePath, new Uint8Array(arrayBuffer));
+
+    await imageRepository.saveImage({
+        fileName,
+        filePath,
+        fileSize,
+        contentType,
+    })
 }
-
