2482e5c7a3bca0a27a4b8e9d5d60c91f7b81d69d
NAbrain <michal2002.60@gmail.com>
2025-05-16 09:00:34 +0200

    update relations for images schema

--- server/src/database/schema/images.ts
+++ server/src/database/schema/images.ts
@@ -2,9 +2,13 @@ import {integer, pgTable, text, varchar} from "npm:drizzle-orm@0.41.0/pg-core";
 import {relations} from "npm:drizzle-orm@0.41.0";
 import {blogPosts} from "./blogPosts.ts";
 
-export const files = pgTable('files', {
+export const images = pgTable('images', {
     id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
     fileName: varchar('file_name', {length: 50}).notNull(),
     contentType: varchar('content_type', {length: 50}).notNull(),
-    imageDate: text('image_date').notNull()
+    imageData: text('image_data').notNull()
 })
+
+export const imagesRelations = relations(images, ({ many }) => ({
+    blogPosts: many(blogPosts),
+}));
