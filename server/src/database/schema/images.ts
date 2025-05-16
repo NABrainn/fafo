c8377fecf73d46e5ee8caf31e1dd1da8a6f1201f
NAbrain <michal2002.60@gmail.com>
2025-05-16 17:04:22 +0200

    update schema fields

--- server/src/database/schema/images.ts
+++ server/src/database/schema/images.ts
@@ -1,12 +1,15 @@
-import {integer, pgTable, text, varchar} from "npm:drizzle-orm@0.41.0/pg-core";
+import {integer, pgTable, varchar} from "npm:drizzle-orm@0.41.0/pg-core";
 import {relations} from "npm:drizzle-orm@0.41.0";
 import {blogPosts} from "./blogPosts.ts";
+
+export type  Image = typeof images.$inferSelect | typeof images.$inferInsert
 
 export const images = pgTable('images', {
     id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
     fileName: varchar('file_name', {length: 50}).notNull(),
+    filePath: varchar('file_path', {length: 50}).notNull(),
+    fileSize: integer('file_size').notNull(),
     contentType: varchar('content_type', {length: 50}).notNull(),
-    imageData: text('image_data').notNull()
 })
 
 export const imagesRelations = relations(images, ({ many }) => ({
