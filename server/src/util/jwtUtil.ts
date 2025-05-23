1573ea74dae78214f61610894f1486cf54f53b80
NAbrain <michal2002.60@gmail.com>
2025-05-23 16:22:48 +0200

    refactor to use hono's built in api

--- server/src/util/jwtUtil.ts
+++ server/src/util/jwtUtil.ts
@@ -1,24 +1,25 @@
-import { jwtVerify, SignJWT } from "jose";
+import { decode, sign, verify } from 'hono/jwt'
+const secret = Deno.env.get("JWT_SECRET") || ''
 
-const secret = new TextEncoder().encode(
-    Deno.env.get("JWT_SECRET")
-)
 const alg = 'HS256'
 
 export const generateJWT = async (username: string) => {
-  return await new SignJWT()
-    .setProtectedHeader({ alg })
-    .setIssuedAt()
-    .setIssuer('blog-post-api-iss')
-    .setAudience('blog-post-api-aud')
-    .setSubject(username)
-    .setExpirationTime('24h')
-    .sign(secret)
+  const payload = {
+    sub: username,
+    exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24
+  }
+  const token = await sign(payload, secret, alg);
+  console.log('generated: ',token)
+  return token
 }
 
 export const verifyJWT = async (jwt: string) => {
+  console.log('passed: ',jwt)
+
   try {
-    const { payload } = await jwtVerify(jwt, secret);
+    const payload = await verify(jwt, secret, alg);
+    console.log('verified: ', payload)
+
     return payload;
   } catch (_error) {
     return null;
