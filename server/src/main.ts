5f9f92922e95956b1ab48393f1dc59b4d2fb9560
Hecate-vv <161923574+Hecate-vv@users.noreply.github.com>
2025-06-06 20:05:02 +0200

    is this grass?

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,13 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { blogPostController } from "./controller/blogPostController.ts";
-import { commentController } from "./controller/commentController.ts";
 import { userController } from "./controller/userController.ts";
 import { jwt } from 'hono/jwt'
 import type { JwtVariables } from 'hono/jwt'
 import { authController } from "./controller/authController.ts";
+import { createCommentController } from "./controller/commentController.ts";
+import { CommentRepository } from "./database/repository/commentRepository.ts";
+import { db } from "./database/database.ts";
 
 type Variables = JwtVariables
 
@@ -32,9 +34,11 @@ app.use('/api/*', jwt({
     alg: 'HS256',
 }))
 
+const commentController = createCommentController(new CommentRepository(db));
+
 app.route('/api/posts', blogPostController);
 app.route('/api/comments', commentController);
 app.route('/api/users', userController);
 app.route('/auth', authController);
 
-Deno.serve(app.fetch)
+Deno.serve(app.fetch);
